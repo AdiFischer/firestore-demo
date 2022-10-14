7a2b6fdf43ad5564bd4a6d523b938a770e610146
AdiFischer <adifischer1@gmail.com>
2022-10-14 12:58:57 -0400

    firestore-demo

---
+++ serviceAccount.js
@@ -0,0 +1,12 @@
+export default {
+  "type": "service_account",
+  "project_id": "petstore-837c0",
+  "private_key_id": "975fb06d073d578ba8f0bfb0c5cea12aea00d545",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC1k1KqBdmmc5+f\ncJ9lGH/R6Z2gd/zeznXLAeNls4ba6hxf7OktVyh4LPapy7gSSdzKqUwYDBNl2LSx\nVBJIrX3SE8UlSfw/+Oo1aeITtWqczz97ZFLbT8gddcByFoKm7DI8fr8GExAzRBEP\n5FZ5n0yKpX/AogJQW2rZeGt6aeDXJ2oaIgNX60z/BOHX2tQmDq6z0wCCuowGj+UB\nbQGtUIu9yqKDdjggh9sLCxQ/FMC+WOv5tFw11+AXfXdpr3Cw4j9R6Nt3EeaBZ7AS\nkUyDA9D/iOZsz/MywyFecMpylEmygR4e8Nl/hz3K08+nWwfR7UagqBQJLTWZP5mz\nR1w/WKyFAgMBAAECgf9viE7+oRlQZTVneoZuUt4w0o9AQnPRaIBycnZ7wgrT7XoA\nckXI1ADH0Nay9a/bjANTeSqEmuZPaQ2U309b1TW0FPDENu37Xap/pMp50LXsHGGi\nKb27eQYzEmFjsvAOGIfDIRe1Y4BP/T99w4RasL8TaxVN/jXtpjm723wpdV/myAOK\nakOlgULoEXv/Xskg/6ulaqZeaC5vEmeRmrmVAfG1Vq65ISsXJ47QtSvHZ4Ci9ypP\nifdtxKYEUMpYEXOnRxeQqbeMfvtcvhCM8G0EtphaZBF8JuiQFHhFRHNkRKNcTmO3\n7HcosvKGek8+firaOD6jrI3obgp9e9MgJ8sdtakCgYEA+OJOwUZZ4NBw4wDezmq0\nuNuBdH2eRDO9hD5BZhDmGHpeRA4RwJ1SrnOqujTSzO3t/FM1j/u1xGZ7uR16WS8O\n1PCXja22vGEthN/uYY6wmT7ylNfzt503WQpVKAZdDdYMxWxWEf1VVSQIerzeOYtA\nFQxt1GmSFFXg0A0J8xEcJVkCgYEAusRasrS972DqdMLfElmd2R6WQRNDRTvSO+dg\n2mRHZ+xF4dFFtj0WAEcsR1cOnq+A1FbMfVKT8OZhs3S66zDeATlshTM8LtVhxCh0\njwrj7Fmj4KOPe/JcHGmzFVfqq5NJNG9Y9CS5rA0SavCB1XJoqQ7di5kerC3CZVnB\n0O3tHw0CgYEAj3J4a+NteCsLdXsR8C0GXql9krO4XZzotWjgbsB8rk0ROe5WAwtq\nFUxrcYlymlTkXxNigstUN4/N7aL02qRJyyekYi9mbGsiPjyG+8mvNZRXYryZJxJB\nTVusOzZ3ubrOf5dygEngPgNXs85Z0xbUZ41TXCNA4+GfgqqROnx5GXkCgYEAoFXN\nHzm3DPgAjzh0VMaGkkyF5VvqFleixjZ+mT4ZZwgZfiEwXaDXcGmpg50jOeulB6V0\na+omg1rkaTE+/oW3MQnhNgCkG/ins8SfO+W3gcH6w/DmTxvjI6lCSoJtPdp1sAQf\nM4kLtL6MHy5V564OV4QhgaR0BpBKIMQLdZesR0kCgYBX41Qbm34Ide6O8MuF8vhR\nMCJaAsWGjrjULq3U7JHo1uuaLAzMPsYBMQbPOIE53SW5Hu2uR4Puop9Cfc9soK5f\ntZvcox/zMWJS33c9wNzpgOAopptXikP3Sl0stcZ6OwVSRXrlxctLdirlSR1/MmLB\n45llU4BOER3RQbjT37kcqQ==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-xsuni@petstore-837c0.iam.gserviceaccount.com",
+  "client_id": "105261299717408965394",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xsuni%40petstore-837c0.iam.gserviceaccount.com"
+}
